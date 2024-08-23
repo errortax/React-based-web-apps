6d0895814388fb4a3f08ab06b91652cbe164aed5
tasmia monzoor <tasmiamonzoor@gmail.com>
2024-08-23 13:04:26 +0600

    new handler added

--- my_app/src/Mycomponent.jsx
+++ my_app/src/Mycomponent.jsx
@@ -3,15 +3,26 @@
 import React ,{useState} from "react";
 
 function Mycomponent(){
- const [name, setName] = useState("");
+ const [name, setName] = useState("Guest");
+ const [quantity, setQuantity] = useState(0);
  
  function handleName(event){
     setName(event.target.value);
  }
 
+ function handleQuantity(event){
+    setQuantity(event.target.value);
+ }
+
+
+
+
  return(<div>
-    <input value={name} onChange={handleName}></input>
+    <input value={name} onChange={handleName} type="text"></input>
     <p>Name: {name}</p>
+    <input value={quantity} onChange={handleQuantity} type="number"></input>
+    <p>Quantity: {quantity}</p>
+
  </div>);
 
 
