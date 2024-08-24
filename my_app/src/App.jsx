2bc315880d547a8ce1a519c660f1afc347d604c5
tasmia monzoor <tasmiamonzoor@gmail.com>
2024-08-24 14:20:06 +0600

    new lists added

--- my_app/src/App.jsx
+++ my_app/src/App.jsx
@@ -13,10 +13,12 @@ import Mycomponent from "./Mycomponent";
 import ColorPicker from "./ColorPicker";
 import UpdateState from "./UpdateState";
 import ArrayState from "./ArrayState";
+import CarList from "./CarList";
 
 function App(){
   //return(<Mycomponent />);
-  return(<ArrayState />);
+ // return(<ArrayState />);
+ return (<CarList />)
 }
 
 export default App;
